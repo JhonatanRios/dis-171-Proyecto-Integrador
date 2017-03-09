e930f1311deef87502365e48dc63ee3953964468
Jhonatan Rios <Jhonatan Rios@Internet>
2017-03-09 00:07:39 -0500

    agregar get y set

--- src/Persona.java
+++ src/Persona.java
@@ -35,6 +35,29 @@ public class Persona {
 				+ " // Peso: " + peso, app.width / 2, y);
 	}
 
+	public int compareTo(Persona p) {
+		if (this.apellido.equals(p.getApellido())) {
+			return 0;
+		}
+		return this.apellido.compareTo(p.getApellido());
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
 	public int getEdad() {
 		return edad;
 	}
